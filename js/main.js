2f34cd490c79eef22e3e1242c053ae5ca6ec9b92
NikolayOstras <92928230+NikolayOstras@users.noreply.github.com>
2022-04-16 08:45:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -147,58 +147,71 @@ swiper.on('slideChange', function () {
 // ------- //
 
 // drop-down menu
-const dropLink = document.querySelector('.drop__link')
-const dropList = document.querySelector('.drop__list')
-dropLink.onclick = function () {
-    dropLink.classList.toggle('drop__link--active')
-    dropList.classList.toggle('drop__list--active')
 
-}
-// ----- //
-// filter
-const filterItems = document.querySelectorAll('.grid__item')
-function filter () {
-  dropList.addEventListener('click', e => {
-    const targetId = e.target.dataset.filter
-   
+function select(selector, filterTrue) {
+  const dropDown = document.querySelector(selector)
+  let filterItems = 0
+  //filter init
+  if (filterTrue) {
+     const filterList = dropDown.querySelector('.dropdown__filter')
+     if (filterList !== 0) {
+        filterItems = filterList.querySelectorAll('[filter]')
+     }
 
-    switch(targetId) {
-    
-      case 'filter-web':
-        getFilterItems(targetId)
-        break
-      case 'filter-photo':
-        getFilterItems(targetId)
-        break
-      case 'filter-logo':
-        getFilterItems(targetId)
-        break
-      case 'filter-brand':
-        getFilterItems(targetId)
-        break
-      case 'filter-ui':
-        getFilterItems(targetId)
-        break
-      case 'filter-graphic':
-        getFilterItems(targetId)
-        break
-    }
-  })
+  }
+  if (dropDown !== null) {
+     const dropDownButton = dropDown.querySelector('.dropdown__button')
+     const dropDownList = dropDown.querySelector('.dropdown__list')
+     const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item')
+     const dropDownInput = dropDown.querySelector('.dropdown__input--hidden')
+
+
+
+     //open select list
+     dropDownButton.addEventListener('click', function () {
+        dropDownList.classList.toggle('dropdown__list--visible')
+        this.classList.add('dropdown__button--active') //add active shadow
+     })
+     //open list and select value
+     dropDownListItems.forEach(function (listItem) {
+        listItem.addEventListener('click', function (e) {
+           e.stopPropagation
+           dropDownButton.innerText = this.innerText
+           dropDownButton.focus()
+           dropDownInput.value = this.dataset.value
+           dropDownList.classList.remove('dropdown__list--visible')
+           selectFilterValue(listItem.getAttribute('data-value'))
+        })
+     })
+     //close select when click out
+     document.addEventListener('click', function (e) {
+        if (e.target !== dropDownButton) {
+           dropDownList.classList.remove('dropdown__list--visible')
+           dropDownButton.classList.remove('dropdown__button--active')
+        }
+     })
+     //close select by press key esc or tab
+     document.addEventListener('keydown', function (e) {
+        if (e.key === 'Tab' || e.key === 'Escape') {
+           dropDownList.classList.remove('dropdown__list--visible')
+           dropDownButton.classList.remove('dropdown__button--active')
+        }
+     })
+
+  }
+  function selectFilterValue(filterValue) {
+     filterItems.forEach(function (element) {
+        let value = element.getAttribute('filter')
+        element.style.display = ''
+        if (value !== filterValue) {
+           element.style.display = 'none'
+        }
+        if (filterValue === 'all') {
+           element.style.display = ''
+        }
+
+     })
+  }
 }
-filter ()
-function getFilterItems(className) {
-  filterItems.forEach(item => {
-    if (item.classList.contains(className)) {
-      item.style.display = 'block'
-    }
-    else {
-      item.style.display = 'none'
-    }
-  })
-}
-dropLink.addEventListener('click', e => {
-  filterItems.forEach(item => {
-    item.style.display = 'block'
-  })
-})
+select ('.dropdown' , true)
 // ----- //
